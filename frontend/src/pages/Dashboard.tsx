26a003c5287d1bba15dbe735fbc89636328b1329
Vebjørn Baustad <vBaustad@users.noreply.github.com>
2025-05-01 18:07:06 +0200

    first testing of react/ts/tailwind

---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-blue-500">Dashboard</h1>
+            <p>Welcome to your budgenix Dashboard</p>
+        </div>
+    );
+};
+
+export default Dashboard;
